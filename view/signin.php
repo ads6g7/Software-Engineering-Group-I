0fdf951a0078b31d82a3eac6d2a11859866a4890
ClarkBlumer <cjbq4f@mail.missouri.edu>
2015-04-21 18:32:02 -0500

    Deleted 'remember me' check box
    Simplify logout process for time sake.

--- view/signin.php
+++ view/signin.php
@@ -76,11 +76,6 @@
 			    		<div class="form-group">
 			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
 			    		</div>
-			    		<div class="checkbox">
-			    	    	<label>
-			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
-			    	    	</label>
-			    	    </div>
 			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login"><br>
 			    					    	  	<div class="form-group">
 			    		    <input class="form-control" placeholder="User" name="user" type="text">
