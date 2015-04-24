1151e0e71146a1c0ee97ddfdf955d1b175ae3b29
ClarkBlumer <cjbq4f@mail.missouri.edu>
2015-04-23 22:29:23 -0500

    login.php update
    Anyone wanna check my php/query to see if its correct?

--- controller/login.php
+++ controller/login.php
@@ -1,3 +1,12 @@
+<!--
+	GroupI
+	Software Engineering
+	4/23/15
+	login.php
+	Description: login.php handles all login attempts on index.html. On a successful login, the user is redirected to
+				 either the admin, professor, or applicant dashboards accordingly.
+-->
+
 <?php
 	
 	// Here we are using sessions to propagate the login
@@ -7,18 +16,28 @@
 		exit;
 	}
 
+	$errors = 0; // track errors
+
 	$dbhost = "host=/var/lib/openshift/5527ddbb5973cacee00000e9/postgresql/socket/";
 	$dbuser = "adminup8hec1";
 	$dbpass = "evnEWGkla94u";	
+	$dbname = "groupi";
 	$dbconn = pg_connect($dbhost, $dbname, $dbuser, $dbpass) or die('Unable to connect to database' . pg_last_error());
+
+	if(!$dbconn)
+		echo "Error: Unable to connect to the database server!" . "Error code: " . pg_last_error() . "</p>\n";
+	else
+		echo "Opened database successfully!";
 	
+
 	// Check to see if the user has already logged in
 	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
 	
-	// checks for logged in status. will need to add additional checks for
-	//		applicant
-	//		professor
-	//		admin
+	/* checks for logged in status. will need to add additional checks for
+			applicant
+			professor
+			admin
+	*/
 	if ($loggedIn) {
 		//Temporary dashboard redirect
 		header("Location: http://groupi-softwareeng.rhcloud.com/applicantdashboard.html");
@@ -36,10 +55,34 @@
 	
 	//Still need to check against the DB for password
 	function handle_login() {
-		$username = empty($_POST['username']) ? '' : $_POST['username'];
-		$password = empty($_POST['password']) ? '' : $_POST['password'];
+		$username = empty($_POST['username']) ? '' : stripslashes($_POST['username']); // if(username is empty) $username = ''; else $username = stripslashes($_POST['username']);
+		$password = empty($_POST['password']) ? '' : stripslashes($_POST['password']);
 		$failedQuery = "Failed check query";
+		
 		// Check for professor login
+		if($errors == 0)
+		{
+			$query = "SELECT username FROM users.professors WHERE username = '" . $1 . "' and '" . "SELECT password FROM users.authentication WHERE password_hash = '" . $2 . "'";
+			$result = pg_prepare($dbconn, "Check", $query) or die("Invalid query" . pg_last_error());
+			$found = pg_execute($dbconn, "Check", array($username, $password)) or die("Couldn't authenticate username or password: " . pg_last_error());
+			if($found == 0)
+			{
+				echo "<p>The username or password combination entered is not valid.</p>\n";
+				++$errors;
+
+			}
+			else
+			{
+				//direct them to professor dashboard
+				header("Location: http://groupi-softwareeng.rhcloud.com/professordashboard.html");
+			}
+		}
+		
+		if($errors > 0)
+			echo "<p>Please use your browser's BACK button to return to the form and fix the errors indicated. </p>";
+			//header("Location: http://groupi-softwareeng.rhcloud.com/");
+
+		/* 
 		$query = "SELECT username FROM users.professors WHERE username = $1";
 		
 		$result = pg_prepare($dbconn, "Check", $query);
@@ -49,6 +92,7 @@
 			 
 		}
 		else if()
+		*/
 
 		/*$result = pg_query($dbconn, $query) or die($failedQuery . pg_last_error());
 		while()*/
@@ -70,6 +114,6 @@
 		$error = "";
 		require "signin.php";
 	}
-	pg_close($dbconn);
-	
+
+	pg_close($dbconn);	
 ?>
