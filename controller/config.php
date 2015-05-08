caba0ac2075c0090b7a9e39d735e7a1d185676d6
ads6g7 <ads6g7@mail.missouri.edu>
2015-05-08 11:17:11 -0500

    just removing these files that were placed into different folder

--- controller/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define(DB_HOST, "host=http://groupi-softwareeng.rhcloud.com/var/lib/openshift/5527ddbb5973cacee00000e9/postgresql/socket/");
-	define(DB_USER, "user=adminup8hec1");
-	define(DB_PASS, "password=evnEWGkla94u");
-	define(DB_NAME, "dbname=groupi");
-?>
